dd518067a2bc856409f4a3977a6da9f19d45798d
Justin McLaren <justinmclaren@Justins-MacBook-Pro.local>
2017-12-08 13:09:36 -0800

    models created

---
+++ public/models/Article.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+// reference to Schema constructor
+const Schema = mongoose.Schema;
+
+// create new UserSchema object
+const ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  URL: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: false
+  },
+  comment: {
+    type: Schema.Types.ObjectId,
+    ref: 'Comment'
+  }
+});
+
+const Article = mongoose.model('Article', ArticleSchema);
+
+module.exports = Article;
